7e6a6bb1a3111d1eaa7a14d6908719a8afe847f0
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-04-14 15:15:12 +0200

    Run on git log not show

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,7 +1,7 @@
-ROOT="."
-if [ "$#" -eq 1 ]; then
+ROOT=.
+if [ "$#" -eq 2 ]; then
 	ROOT=$1
 fi
 
-VERSION=$(git show ${ROOT} | grep commit | sed "s/^commit //g")
+VERSION=$(git log $ROOT | grep commit | sed "s/^commit //g")
 echo $VERSION
