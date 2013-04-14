8ce9b565cf83876971f5a534005e028e1b2719d5
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-04-14 21:09:07 +0200

    Maybe counting number of commits was better. Now grep for the hash of the last commit.

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,5 +3,5 @@ if [ "$#" -eq 2 ]; then
 	ROOT=$1
 fi
 
-VERSION=$(git log $ROOT | grep commit | sed "s/^commit //g")
+VERSION=$(git log $ROOT | grep commit | head -n 1 | sed "s/^commit //g")
 echo $VERSION
