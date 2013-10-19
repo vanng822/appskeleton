573f4793ab285084baed94c7500e1b061d83bd7c
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-10-19 15:16:13 +0200

    Support mapping of http method and setup for express. Setup this.route for each subclass and define GET, POST, … for handling request.

--- app/routehandlers/base.js
+++ app/routehandlers/base.js
@@ -1,11 +1,32 @@
+var methods = ['GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'TRACE', 'CONNECT', 'OPTIONS', 'ALL'];
 
 var Base = function(app) {
+	this.route = null;
 	this.setup(app);
+	this.routeSetup(app);
 };
 Base.METATAGS_VARNAME = 'metaTags';
 Base.prototype = {
 	setup : function(app) {
 		throw new Error('Please implement setup');
+	},
+	routeSetup: function(app) {
+		var self = this, count = 0;
+		if (self.route) {
+			methods.forEach(function(method) {
+				var lMethod = method.toLowerCase();
+				if (typeof self[method] === 'function') {
+					app[lMethod](self.route, self[method].bind(self));
+					count++;
+				} else if (typeof self[lMethod] == 'function') {
+					app[lMethod](self.route, self[lMethod].bind(self));
+					count++;
+				}
+			});
+			if (count < 1) {
+				console.log(self.route, 'specified as route but no match for any method: ', methods.join(', '));
+			}
+		}
 	},
 	getMetaTags : function(res) {
 		var metaTags = res.locals[Base.METATAGS_VARNAME];
