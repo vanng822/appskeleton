d97955cf60631a99a6cecc680c374fd10f8aecd4
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-11-23 21:52:15 +0100

    Add posibility to run only worker for debug

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-var pstarter = require('pstarter');
-
-pstarter.startMaster(__dirname + '/config', {}, function() {
-	var config = require('./config');
-	pstarter.statServer(config.http.statPort, config.http.statHost);
-	if (process.env['NODE_ENV'] && process.env['NODE_ENV'] === 'development') {
-		pstarter.startWatch(__dirname, [__dirname +'/node_modules'], ['.js', '.json', '.html', '.css']);
-	}
-}).startWorker(function() {
+var worker = function() {
 	var config = require('./config');
 	var express = require('express');
 	var app = express();
@@ -17,10 +9,27 @@ pstarter.startMaster(__dirname + '/config', {}, function() {
 	bootstrap.bootstrap(app);
 	bootstrap.postrun();
 	server.listen(config.http.port, config.http.host);
-	/* Counting request */
-	server.on('request', function(req, res) {
-		process.send({
-			cmd : pstarter.cmd.requestCount
+};
+
+/* run only one single process and worker for debug */
+if('worker' in process.env) {
+	worker();
+} else {
+	var pstarter = require('pstarter');
+	pstarter.startMaster(__dirname + '/config', {}, function() {
+		var config = require('./config');
+		pstarter.statServer(config.http.statPort, config.http.statHost);
+		if(process.env['NODE_ENV'] && process.env['NODE_ENV'] === 'development') {
+			pstarter.startWatch(__dirname, [__dirname + '/node_modules'], ['.js', '.json', '.html', '.css']);
+		}
+
+	}).startWorker(function() {
+		worker();
+		/* Counting request */
+		server.on('request', function(req, res) {
+			process.send({
+				cmd : pstarter.cmd.requestCount
+			});
 		});
 	});
-});
+}
