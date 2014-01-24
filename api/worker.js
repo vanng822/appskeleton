d6621dbfb9cc92e88dd4a841d67e2cef50dd7f61
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-01-24 21:28:25 +0100

    No postrun here

--- api/worker.js
+++ api/worker.js
@@ -1,12 +1,11 @@
-var appRequire = require('app-require');
 var worker = module.exports = function() {
+	var appRequire = require('app-require');
 	var config = appRequire.requireConfig();
 	var restify = require('restify');
 	var server = restify.createServer();
 	var bootstrap = require('./bootstrap.js');
 	bootstrap.setupApp(server, __dirname);
 	bootstrap.bootstrap(server);
-	bootstrap.postrun();
 	// change
 	server.listen(config.http.port, config.http.host);
 	return server;
