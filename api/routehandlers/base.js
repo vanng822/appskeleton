ddde8a643e5cd14c56860926c1ed6fd632fc5362
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-01-26 22:27:00 +0100

    Adding response for OPTIONS, seems that restify break the werbs pattern use \"del\" and \"opts\" instead

--- api/routehandlers/base.js
+++ api/routehandlers/base.js
@@ -1,30 +1,36 @@
-var methods = ['GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'TRACE', 'CONNECT', 'OPTIONS', 'ALL'];
+var methods = ['GET', 'POST', 'HEAD', 'PUT', 'DEL', 'PATCH', 'OPTS'];
 
 var Base = function(app) {
 	this.route = null;
 	this.setup(app);
 	this.routeSetup(app);
 };
-Base.METATAGS_VARNAME = 'metaTags';
+
 Base.prototype = {
 	setup : function(app) {
 		throw new Error('Please implement setup');
 	},
 	routeSetup: function(app) {
-		var self = this, count = 0;
+		var self = this, supportedMethods = [];
 		if (self.route) {
 			methods.forEach(function(method) {
 				var lMethod = method.toLowerCase();
 				if (typeof self[method] === 'function') {
 					app[lMethod](self.route, self[method].bind(self));
-					count++;
+					supportedMethods.push(method);
 				} else if (typeof self[lMethod] === 'function') {
 					app[lMethod](self.route, self[lMethod].bind(self));
-					count++;
+					supportedMethods.push(method);
 				}
 			});
-			if (count < 1) {
+			
+			if (supportedMethods.length < 1) {
 				console.log(self.route, 'specified as route but no match for any method: ', methods.join(', '));
+			} else {
+				app['opts'](self.route, function(req, res, next) {
+					res.header('Allow', supportedMethods.join(', '));
+					return res.send(200, {});
+				});
 			}
 		}
 	}
