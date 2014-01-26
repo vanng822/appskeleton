72d6c41368d889db3ec43c753ec1c7e4360a328d
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-01-26 22:35:58 +0100

    Bugfix, wrong comparison

--- api/routehandlers/base.js
+++ api/routehandlers/base.js
@@ -27,7 +27,7 @@ Base.prototype = {
 			if (supportedMethods.length < 1) {
 				console.log(self.route, 'specified as route but no match for any method: ', methods.join(', '));
 			} else {
-				if (supportedMethods.indexOf('OPTS') != -1) {
+				if (supportedMethods.indexOf('OPTS') == -1) {
 					app['opts'](self.route, function(req, res, next) {
 						res.header('Allow', supportedMethods.join(', '));
 						return res.send(200, {});
